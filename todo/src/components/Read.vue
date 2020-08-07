859df7c30b0bb74846cea7b139db710878e14e0f
changgoo <cck981007@gmail.com>
2020-08-07 23:41:18 +0900

    read 컴포넌트 생성

---
+++ todo/src/components/Read.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Read
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Read'
+}
+</script>
---
+++ todo/src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    writer: '아이린',
+    title: '레드벨벳',
+    content: '1'
+  },
+  {
+    writer: '슬기',
+    title: '레드벨벳',
+    content: '2'
+  },
+  {
+    writer: '이나은',
+    title: '에이프릴',
+    content: '3'
+  }
+]
